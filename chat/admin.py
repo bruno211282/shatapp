8a67068e70825d0ef20d1e0564469d2fa601d6b9
Bruno Garcia <bgarcia@ascentio.com.ar>
2022-07-01 20:26:18 -0300

    Create models and register in admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from chat import models
 
 # Register your models here.
+admin.site.register(models.ChatMessage)
+admin.site.register(models.ChatRoom)
+admin.site.register(models.ChatUser)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ChatUser(User):
+    rooms_im_in = models.ForeignKey(
+        to="ChatRoom", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+
+class ChatRoom(models.Model):
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    created_by = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    room_name = models.CharField(max_length=50)
+    room_topic = models.TextField()
+
+    def __str__(self) -> str:
+        return self.room_name
+
+
+class ChatMessage(models.Model):
+    received_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    text = models.TextField()
+    room = models.ForeignKey(to=ChatRoom, on_delete=models.CASCADE)
+    from_user = models.ForeignKey(to=User, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        ordering = ["received_at"]
